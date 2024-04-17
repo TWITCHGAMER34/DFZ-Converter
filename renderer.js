a413abb53600515f037ff3c894c609271e1d3516
Zoltan Nagy <nazoma05@gmail.com>
2024-04-17 14:47:52 +0200

    added space to the JSON-data

--- renderer.js
+++ renderer.js
@@ -2,19 +2,16 @@ window.addEventListener('DOMContentLoaded', () => {
     const axios = require('axios');
 
     function customLog(message) {
-        console.log(message); // This will log the message to the console
 
-        // This will append the message to the 'console-log' div in your HTML
+
         document.getElementById('console-log').innerHTML += message + '<br>';
     }
 
     axios.get('https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/eur.json')
         .then(response => {
-            customLog(JSON.stringify(response.data)); // This will log the data returned by the API to the console and the HTML
-            console.log(response.data);
+            customLog(JSON.stringify(response.data, null, 2));
         })
         .catch(error => {
-            customLog(error); // This will log any error that occurs during the request to the console and the HTML
-            console.log(error);
+            customLog(error);
         });
 });
